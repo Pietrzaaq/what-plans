cea8156c329cf7da2e0a25e6acf6887a99c52a38
Dawid Pietrzak <pietrzakd53@gmail.com>
2024-03-11 23:57:56 +0100

    Added new entities

--- backend/WhatPlans.Domain/Entities/Organizer.cs
+++ backend/WhatPlans.Domain/Entities/Organizer.cs
@@ -1,8 +1,9 @@
 ﻿namespace WhatPlans.Domain.Entities;
 
-public class Artist
+public class Organizer
 {
     public Guid Id { get; set; }
-    public Guid PersonId { get; set; }
+    public Guid? PersonId { get; set; }
+    public Guid? CompanyId { get; set; }
     public string Name { get; set; }
 }
