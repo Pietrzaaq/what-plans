747864b688a087b50d743dc77bcb81eae82329db
Dawid Pietrzak <pietrzakd53@gmail.com>
2024-10-18 00:08:49 +0200

    Added missing request

---
+++ backend/WhatPlans.Application/Users/Create/Request.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhatPlans.Application.Users.Create;
+
+public class Request : IRequest<IdentityResult>
+{
+    [FromBody]
+    public Data Body { get; set; }
+
+    public class Data
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Password { get; set; }
+        public string Culture { get; set; }
+    }
+}
