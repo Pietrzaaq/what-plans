ba6574cb4e2585d7732cb0222fd651558c51c2a7
Dawid Pietrzak <pietrzakd53@gmail.com>
2024-10-18 19:43:27 +0200

    Fixed frontend .env variables
    (cherry picked from commit b0d77dd75f6a6c4b8456f63b504dd1f903febe62)

--- frontend/src/services/placesService.js
+++ frontend/src/services/placesService.js
@@ -2,19 +2,19 @@
 export default new class PlacesService {
 
     async getAll() {
-        return await axios.get('https://localhost:5000/api/places').then(response => response.data);
+        return await axios.get(`${import.meta.env.VITE_API_BASE_URL}/places`).then(response => response.data);
     }
 
     async getAllWithEvents() {
-        return await axios.get(`https://localhost:5000/api/places/events`).then(response => response.data);
+        return await axios.get(`${import.meta.env.VITE_API_BASE_URL}/places/events`).then(response => response.data);
     }
 
     async getEventsByPlaceId(placeId) {
-        return await axios.get(`https://localhost:5000/api/places/${placeId}/events`).then(response => response.data);
+        return await axios.get(`${import.meta.env.VITE_API_BASE_URL}/places/${placeId}/events`).then(response => response.data);
     }
 
     async create(place) {
-        return await axios.post('https://localhost:5000/api/places', place);
+        return await axios.post(`${import.meta.env.VITE_API_BASE_URL}/places`, place);
     }
 };
 
